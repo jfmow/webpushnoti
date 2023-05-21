e878a93c5c9558664ad67f8cf4653c38ddc8265c
sudsymoss <james1mowat@gmail.com>
2023-05-21 20:03:11 +1200

    sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,16 +1,49 @@
+const CACHE_NAME = 'my-pwa-cache';
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    // Perform any necessary installation steps here
-    // (e.g., caching static assets)
-    console.log('Service worker installed')
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll([
+          '/', // Add the URLs of your static assets here
+          '/favicon.ico',
+          '/manifest.json',
+          // Include other static assets such as images, icons, etc.
+        ]);
+      })
+      .then(() => {
+        console.log('Service worker installed');
+      })
   );
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    // Perform any necessary activation steps here
-    // (e.g., clearing old caches)
-    console.log('Service worker activated')
+    caches.keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.filter((name) => {
+            return name !== CACHE_NAME;
+          }).map((name) => {
+            return caches.delete(name);
+          })
+        );
+      })
+      .then(() => {
+        console.log('Service worker activated');
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response; // Serve the cached version if available
+        }
+        return fetch(event.request); // Otherwise, fetch from the network
+      })
   );
 });
 
