59359e156a31b0aaf1b692e361031ce9c58a1652
sudsymoss <james1mowat@gmail.com>
2023-05-23 16:44:56 +1200

    sv

--- pages/articles/[id].js
+++ pages/articles/[id].js
@@ -77,7 +77,7 @@ export default function Viewer({ arti }) {
         }
     }
     if (isAdmin || (isAdmin && isAuthor)) {
-        return <><SavedData userSaved={pb.authStore.model.saved_articles.includes(artiArticle.id)} savedData={articleData2} articleArti={artiArticle} /><Comments articleId={arti.article} /><div className={styles.admineditbtns}>
+        return <><SavedData savedData={articleData2} articleArti={artiArticle} /><Comments articleId={arti.article} /><div className={styles.admineditbtns}>
             <Link href={`/editor/${arti.article}`}><button className={styles.submitbutton} type="button">Edit article</button></Link>
             <button onClick={adminPublishArticle} className={styles.publishbutton}>{isPublished ? "Unpublish" : "Publish article"}</button>
         </div><ReportBtn articleName={artiArticle} /></>
@@ -86,7 +86,7 @@ export default function Viewer({ arti }) {
         console.log(artiArticle)
         return <>
 
-            <SavedData savedData={articleData2} userSaved={pb.authStore.model.saved_articles.includes(artiArticle.id)} articleArti={artiArticle} /><Comments articleId={arti.article} /><div className={styles.admineditbtns}>
+            <SavedData savedData={articleData2} articleArti={artiArticle} /><Comments articleId={arti.article} /><div className={styles.admineditbtns}>
                 {artiArticle?.author === pb.authStore.model.id && (
                     <Link href={`/editor/${arti.article}`}><button className={styles.submitbutton} type="button">Edit article</button></Link>
                 )}
@@ -118,13 +118,18 @@ export default function Viewer({ arti }) {
         </div>)
     }
 
-    return <><SavedData savedData={articleData2} userSaved={pb.authStore.model.saved_articles.includes(artiArticle.id)} articleArti={artiArticle} /><Comments articleId={arti.article} /><ReportBtn articleName={artiArticle} /></>
+    return <><SavedData savedData={articleData2} articleArti={artiArticle} /><Comments articleId={arti.article} /><ReportBtn articleName={artiArticle} /></>
 }
 
-const SavedData = ({ savedData, articleArti, userSaved }) => {
-    const [userSavedAlt, setUserSaved] = useState(userSaved)
-    const [shareModal, setShareModal]= useState(false);
-    
+const SavedData = ({ savedData, articleArti }) => {
+    const [userSavedAlt, setUserSaved] = useState(false)
+    const [shareModal, setShareModal] = useState(false);
+    useEffect(() => {
+        if (pb.authStore.isValid) {
+            const fff = pb.authStore.model.saved_articles.includes(articleArti.id)
+            setUserSaved(fff)
+        }
+    }, [articleArti])
     async function saveArticle() {
         if (!pb.authStore.isValid) {
             return window.location.replace(`/auth/login/articles/${articleArti.id}`)
@@ -189,7 +194,7 @@ const SavedData = ({ savedData, articleArti, userSaved }) => {
 
         // Optionally, provide visual feedback to the user
         setShareModal(false)
-      }
+    }
     const { time, blocks, version } = savedData;
     return (
         <div className={styles.container}>
@@ -206,7 +211,7 @@ const SavedData = ({ savedData, articleArti, userSaved }) => {
                     <h4>Written by: <Link className={styles.authorlink} href={`/u/@${articleArti.expand?.author?.username}`}>{articleArti.expand?.author?.username || "unknown"}</Link></h4>
                     <p>On the {new Date(time).toLocaleDateString()}</p>
                     <div className={styles.sharebtns_container}>
-                        <button className={styles.sharebtn_button} onClick={()=>setShareModal(true)} type="button"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="M720 976q-50 0-85-35t-35-85q0-7 1-14.5t3-13.5L322 664q-17 15-38 23.5t-44 8.5q-50 0-85-35t-35-85q0-50 35-85t85-35q23 0 44 8.5t38 23.5l282-164q-2-6-3-13.5t-1-14.5q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35q-23 0-44-8.5T638 384L356 548q2 6 3 13.5t1 14.5q0 7-1 14.5t-3 13.5l282 164q17-15 38-23.5t44-8.5q50 0 85 35t35 85q0 50-35 85t-85 35Zm0-640q17 0 28.5-11.5T760 296q0-17-11.5-28.5T720 256q-17 0-28.5 11.5T680 296q0 17 11.5 28.5T720 336ZM240 616q17 0 28.5-11.5T280 576q0-17-11.5-28.5T240 536q-17 0-28.5 11.5T200 576q0 17 11.5 28.5T240 616Zm480 280q17 0 28.5-11.5T760 856q0-17-11.5-28.5T720 816q-17 0-28.5 11.5T680 856q0 17 11.5 28.5T720 896Zm0-600ZM240 576Zm480 280Z" /></svg></button>
+                        <button className={styles.sharebtn_button} onClick={() => setShareModal(true)} type="button"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24"><path d="M720 976q-50 0-85-35t-35-85q0-7 1-14.5t3-13.5L322 664q-17 15-38 23.5t-44 8.5q-50 0-85-35t-35-85q0-50 35-85t85-35q23 0 44 8.5t38 23.5l282-164q-2-6-3-13.5t-1-14.5q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35q-23 0-44-8.5T638 384L356 548q2 6 3 13.5t1 14.5q0 7-1 14.5t-3 13.5l282 164q17-15 38-23.5t44-8.5q50 0 85 35t35 85q0 50-35 85t-85 35Zm0-640q17 0 28.5-11.5T760 296q0-17-11.5-28.5T720 256q-17 0-28.5 11.5T680 296q0 17 11.5 28.5T720 336ZM240 616q17 0 28.5-11.5T280 576q0-17-11.5-28.5T240 536q-17 0-28.5 11.5T200 576q0 17 11.5 28.5T240 616Zm480 280q17 0 28.5-11.5T760 856q0-17-11.5-28.5T720 816q-17 0-28.5 11.5T680 856q0 17 11.5 28.5T720 896Zm0-600ZM240 576Zm480 280Z" /></svg></button>
                         {userSavedAlt ? (
                             <button onClick={unsaveArticle} className={styles.sharebtn_button} type="button"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M713-600 600-713l56-57 57 57 141-142 57 57-198 198ZM200-120v-640q0-33 23.5-56.5T280-840h280q-20 30-30 57.5T520-720q0 72 45.5 127T680-524q23 3 40 3t40-3v404L480-240 200-120Z" /></svg></button>
                         ) : (
@@ -215,14 +220,14 @@ const SavedData = ({ savedData, articleArti, userSaved }) => {
                     </div>
                     {shareModal && (
                         <>
-                            <div className={styles.sharemodal_container} onClick={()=>setShareModal(false)}>
+                            <div className={styles.sharemodal_container} onClick={() => setShareModal(false)}>
                                 <div className={styles.shareModal} onClick={(event) => event.stopPropagation()}>
                                     <h2>Share {articleArti.title}</h2>
                                     <div className={styles.shareModal_link}>
                                         <div className={styles.shareModal_link_text}>
                                             https://news.suddsy.dev/articles/{articleArti.id}
                                         </div>
-                                        <button onClick={copyToClip} className={styles.shareModal_link_btn}><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-160q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Z"/></svg></button>
+                                        <button onClick={copyToClip} className={styles.shareModal_link_btn}><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-160q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Z" /></svg></button>
                                     </div>
                                 </div>
                             </div>
